9878e326101dc5551dc57b1a82bb3a2db61a0ef7
Swapnil Haria <swapnilster@gmail.com>
2019-02-19 10:58:55 -0800

    Adding unique store count

--- lackey/lackey.h
+++ lackey/lackey.h
@@ -7,7 +7,9 @@ typedef
    enum {
       VG_USERREQ__DUMP_STATS = VG_USERREQ_TOOL_BASE('L','K'),
       VG_USERREQ__START_INSTRUMENTATION,
-      VG_USERREQ__STOP_INSTRUMENTATION
+      VG_USERREQ__STOP_INSTRUMENTATION,
+      VG_USERREQ__OPERATION_START,
+      VG_USERREQ__OPERATION_END
    } Vg_CallgrindClientRequest;
 
 // Starts full instrumentation if not already switched on.
@@ -20,6 +22,14 @@ typedef
   VALGRIND_DO_CLIENT_REQUEST_STMT(VG_USERREQ__STOP_INSTRUMENTATION, \
                                   0, 0, 0, 0, 0)
 
+// Indicates start of persistent operation.
+#define LACKEY_OPERATION_START\
+  VALGRIND_DO_CLIENT_REQUEST_STMT(VG_USERREQ__OPERATION_START, \
+                                  0, 0, 0, 0, 0)
 
+// Indicates end of persistent operation.
+#define LACKEY_OPERATION_END\
+  VALGRIND_DO_CLIENT_REQUEST_STMT(VG_USERREQ__OPERATION_END, \
+                                  0, 0, 0, 0, 0)
 
 #endif /* __LACKEY_H */
